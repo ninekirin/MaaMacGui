d4bc48bef1023b9a85bb07f26e576400f8d9f086
KirinBaka <bakaawt@hotmail.com>
2025-05-07 22:45:11 +0800

    feat: CopilotList prototype
    目前已知会影响战斗列表的问题：
    
    - Mac 下的跳过剧情有概率不工作，导致中途卡在跳过剧情页
    - 原有的 copilots 列表并不是在程序启动后预载，而是列表页面加载时从 copilot 和 recording 目录读取，没有持久化设计，导致每次切换都会重新读取目录，导致一些问题
    - 删除按钮应统一到原有的 copilots 列表项中，否则容易造成歧义（点击按钮删的是战斗列表还是作业列表项呢）
    - 启动任务失败
    - 切到战斗列表模式后，任务 Log 页面的按钮点击无反应

--- MeoAsstMac/Navigation/CopilotContent.swift
+++ MeoAsstMac/Navigation/CopilotContent.swift
@@ -10,70 +10,45 @@ import SwiftUI
 struct CopilotContent: View {
     @EnvironmentObject private var viewModel: MAAViewModel
     @Binding var selection: URL?
-
-    @State private var copilots = Set<URL>()
-    @State private var downloading = false
-    @State private var expanded = false
-
+    
+    private func toggleCopilotList() {
+        viewModel.useCopilotList.toggle()
+    }
+    
     var body: some View {
-        List(selection: $selection) {
-            DisclosureGroup(isExpanded: $expanded) {
-                ForEach(bundledCopilots, id: \.self) { url in
-                    Text(url.lastPathComponent)
-                }
-            } label: {
-                Text("内置作业")
-                    .font(.subheadline)
-                    .fontWeight(.semibold)
-                    .foregroundStyle(.secondary)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .contentShape(Rectangle())
-                    .onTapGesture {
-                        withAnimation {
-                            expanded.toggle()
-                        }
-                    }
-            }
-
-            Section {
-                ForEach(copilots.urls, id: \.self) { url in
-                    Text(url.lastPathComponent)
-                }
-            } header: {
-                HStack {
-                    Text("外部作业（可拖入文件）")
-                    if downloading {
-                        Spacer()
-                        ProgressView().controlSize(.small)
-                    }
-                }
+        Group {
+            if viewModel.useCopilotList {
+                CopilotList()
+            } else {
+                RegularCopilotList(
+                    selection: $selection,
+                    onDeleteCopilot: deleteCopilot
+                )
             }
         }
         .toolbar(content: listToolbar)
-        .animation(.default, value: copilots)
-        .animation(.default, value: downloading)
-        .onAppear(perform: loadUserCopilots)
-        .onDrop(of: [.fileURL], isTargeted: .none, perform: addCopilots)
-        .onReceive(viewModel.$copilotDetailMode, perform: deselectCopilot)
-        .onReceive(viewModel.$downloadCopilot, perform: downloadCopilot)
-        .onReceive(viewModel.$videoRecoginition, perform: selectNewCopilot)
-        .fileImporter(
-            isPresented: $viewModel.showImportCopilot,
-            allowedContentTypes: [.json],
-            allowsMultipleSelection: true,
-            onCompletion: addCopilots)
     }
-
-    // MARK: - Toolbar
 
     @ToolbarContentBuilder private func listToolbar() -> some ToolbarContent {
         ToolbarItemGroup {
-            Button(action: deleteSelectedCopilot) {
+            Button(action: {
+                if let selection {
+                    deleteCopilot(url: selection)
+                }
+            }) {
                 Label("移除", systemImage: "trash")
             }
             .help("移除作业")
-            .disabled(shouldDisableDeletion)
+            .disabled(!canDelete(selection))
             .keyboardShortcut(.delete, modifiers: [.command])
+
+            Button(action: toggleCopilotList) {
+                Label(
+                    "战斗列表",
+                    systemImage: viewModel.useCopilotList ? "list.bullet.rectangle.fill" : "list.bullet.rectangle"
+                )
+            }
+            .help(viewModel.useCopilotList ? "切换回单个作业模式" : "切换到战斗列表模式")
         }
 
         ToolbarItemGroup {
@@ -96,246 +71,48 @@ struct CopilotContent: View {
             }
         }
     }
-
+    
     // MARK: - Actions
-
+    
     private func stop() {
         Task {
             try await viewModel.stop()
         }
     }
-
+    
     private func start() {
         Task {
             viewModel.copilotDetailMode = .log
             try await viewModel.startCopilot()
         }
     }
-
-    private func loadUserCopilots() {
-        copilots.formUnion(externalDirectory.copilots)
-        copilots.formUnion(recordingDirectory.copilots)
-    }
-
-    private func addCopilots(_ providers: [NSItemProvider]) -> Bool {
-        Task {
-            for provider in providers {
-                if let url = try? await provider.loadURL() {
-                    let value = try? url.resourceValues(forKeys: [.contentTypeKey])
-                    if value?.contentType == .json {
-                        copilots.insert(url)
-                    } else if value?.contentType?.conforms(to: .movie) == true {
-                        try? await viewModel.recognizeVideo(video: url)
-                    }
-                }
-            }
-            self.selection = self.copilots.urls.last
-        }
-
-        return true
-    }
-
-    private func addCopilots(_ results: Result<[URL], Error>) {
-        if case let .success(urls) = results {
-            copilots.formUnion(urls)
-            selection = copilots.urls.last
-        }
-    }
-
-    private func downloadCopilot(id: String?) {
-        guard let id else { return }
-
-        let file =
-            externalDirectory
-            .appendingPathComponent(id)
-            .appendingPathExtension("json")
-
-        let url = URL(string: "https://prts.maa.plus/copilot/get/\(id)")!
-        Task {
-            self.downloading = true
-            do {
-                let data = try await URLSession.shared.data(from: url).0
-                let response = try JSONDecoder().decode(CopilotResponse.self, from: data)
-                try response.data.content.write(toFile: file.path, atomically: true, encoding: .utf8)
-                copilots.insert(file)
-                self.selection = file
-            } catch {
-                print(error)
-            }
-            self.downloading = false
-        }
-    }
-
+    
     private func deleteCopilot(url: URL) {
-        copilots.remove(url)
-        guard canDelete(url) else { return }
-        try? FileManager.default.removeItem(at: url)
-    }
-
-    private func deleteSelectedCopilot() {
-        guard let selection, let index = copilots.urls.firstIndex(of: selection) else { return }
-
-        deleteCopilot(url: selection)
-
-        let urls = copilots.urls
-        if index < urls.count {
-            self.selection = urls[index]
-        } else {
-            self.selection = urls.last
-        }
-    }
-
-    private func deselectCopilot(_ viewMode: MAAViewModel.CopilotDetailMode) {
-        if viewMode != .copilotConfig {
-            selection = nil
-        }
-    }
-
-    private func selectNewCopilot(url: URL?) {
-        if let url {
-            copilots.insert(url)
-            selection = copilots.urls.last
-        }
-    }
-
-    // MARK: - State Wrappers
-
-    private var shouldDisableDeletion: Bool {
-        selection == nil || isBundled(selection)
-    }
-
-    private func isBundled(_ url: URL?) -> Bool {
-        return url?.path.starts(with: bundledDirectory.path) ?? false
-    }
-
-    private func canDelete(_ url: URL?) -> Bool {
-        [externalDirectory, recordingDirectory]
-            .compactMap { url?.path.starts(with: $0.path) }
-            .first(where: { $0 })
-            ?? false
-    }
-
-    // MARK: - File Paths
-
-    private var bundledCopilots: [URL] { bundledDirectory.copilots }
-
-    private var bundledDirectory: URL {
-        Bundle.main.resourceURL!
+        let bundledPath = Bundle.main.resourceURL!
             .appendingPathComponent("resource")
             .appendingPathComponent("copilot")
+            .path
+            
+        if !url.path.starts(with: bundledPath) {
+            try? FileManager.default.removeItem(at: url)
+        }
     }
-
-    private var externalDirectory: URL {
-        let directory = FileManager.default
-            .urls(for: .documentDirectory, in: .userDomainMask)
-            .first!
+    
+    // MARK: - Helpers
+    
+    private func canDelete(_ url: URL?) -> Bool {
+        guard let url else { return false }
+        
+        let bundledPath = Bundle.main.resourceURL!
+            .appendingPathComponent("resource")
             .appendingPathComponent("copilot")
-
-        if !FileManager.default.fileExists(atPath: directory.path) {
-            try? FileManager.default.createDirectory(
-                at: directory, withIntermediateDirectories: true)
-        }
-
-        return directory
-    }
-
-    private var recordingDirectory: URL {
-        FileManager.default
-            .urls(for: .applicationSupportDirectory, in: .userDomainMask)
-            .first!
-            .appendingPathComponent("cache")
-            .appendingPathComponent("CombatRecord")
+            .path
+            
+        return !url.path.starts(with: bundledPath)
     }
 }
 
-struct CopilotContent_Previews: PreviewProvider {
-    static var previews: some View {
-        CopilotContent(selection: .constant(nil))
-            .environmentObject(MAAViewModel())
-    }
-}
-
-// MARK: - Value Extensions
-
-extension URL {
-    fileprivate var copilots: [URL] {
-        guard
-            let urls = try? FileManager.default.contentsOfDirectory(
-                at: self,
-                includingPropertiesForKeys: [.contentTypeKey],
-                options: .skipsHiddenFiles)
-        else { return [] }
-
-        return urls.filter { url in
-            let value = try? url.resourceValues(forKeys: [.contentTypeKey])
-            return value?.contentType == .json
-        }
-        .sorted { lhs, rhs in
-            lhs.lastPathComponent < rhs.lastPathComponent
-        }
-    }
-}
-
-extension Set where Element == URL {
-    fileprivate var urls: [URL] { sorted { $0.lastPathComponent < $1.lastPathComponent } }
-}
-
-// MARK: - Download Model
-
-private struct CopilotResponse: Codable {
-    let data: CopilotData
-
-    struct CopilotData: Codable {
-        let content: String
-    }
-}
-
-// MARK: - Convenience Methods
-
-extension NSItemProvider {
-    @MainActor fileprivate func loadURL() async throws -> URL {
-        let handle = ProgressActor()
-
-        return try await withTaskCancellationHandler {
-            try await withCheckedThrowingContinuation { continuation in
-                let progress = loadObject(ofClass: URL.self) { object, error in
-                    if let error {
-                        continuation.resume(throwing: error)
-                        return
-                    }
-
-                    guard let object else {
-                        continuation.resume(throwing: MAAError.emptyItemObject)
-                        return
-                    }
-
-                    continuation.resume(returning: object)
-                }
-
-                Task {
-                    await handle.bind(progress: progress)
-                }
-            }
-        } onCancel: {
-            Task {
-                await handle.cancel()
-            }
-        }
-    }
-}
-
-private actor ProgressActor {
-    private var progress: Progress?
-    private var cancelled = false
-
-    func bind(progress: Progress) {
-        guard !cancelled else { return }
-        self.progress = progress
-        progress.resume()
-    }
-
-    func cancel() {
-        cancelled = true
-        progress?.cancel()
-    }
+#Preview {
+    CopilotContent(selection: .constant(nil))
+        .environmentObject(MAAViewModel())
 }
