77f2f2c23b19be7ca5f0e6382c817007f4ed2310
KirinBaka <bakaawt@hotmail.com>
2025-05-07 23:48:52 +0800

    fix: hide add button when using copilot list
    hide add button when using copilot list
    simplify Section header in CopilotList
    
    作业统一从 RegularCopilotList 进行下载，且添加作业按钮不在战斗列表激活时显示，移除了战斗列表列表头不需要的 downloading 状态

--- MeoAsstMac/Navigation/CopilotList.swift
+++ MeoAsstMac/Navigation/CopilotList.swift
@@ -9,11 +9,10 @@ import SwiftUI
 
 struct CopilotList: View {
     @EnvironmentObject private var viewModel: MAAViewModel
-    @State private var downloading = false
     
     var body: some View {
         List {
-            Section {
+            Section("战斗列表（自动模式）") {
                 // REMOVEME: \.0 使用枚举的索引（即数组的位置）作为唯一标识
                 // REMOVEME: \.1.name 使用每个元素的 item.name 作为唯一标识
                 ForEach(Array(viewModel.copilotListConfig.items.enumerated()), id: \.0) { index, item in
@@ -55,14 +54,6 @@ struct CopilotList: View {
                 }
                 .onMove { from, to in
                     viewModel.moveCopilotItem(from: from.first ?? 0, to: to)
-                }
-            } header: {
-                HStack {
-                    Text("战斗列表（自动模式）")
-                    if downloading {
-                        Spacer()
-                        ProgressView().controlSize(.small)
-                    }
                 }
             }
         }
